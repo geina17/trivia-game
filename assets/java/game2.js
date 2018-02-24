247315020bad2d5a01360ac3f4360f45c07583fd
geina17 <33171782+geina17@users.noreply.github.com>
2018-02-23 22:52:28 -0700

    Add files via upload

--- assets/java/game2.js
+++ assets/java/game2.js
@@ -1,38 +1,39 @@
 var total = 6;
 var score = 0;
 var wrong = 0;
-var unanswered = 0;
+var unan = 0;
 //runs timer
 var bigben = null;
 
 
 var clock = {
     time: 2,
-	timeleft: 0,
+    timeleft: 0,
+    bigben: null,
 
     countDown: function() {
         clock.time--;
         $("#timer").html(clock.time);
+        clock.stop();
     },
     start: function() {
-        bigben = setInterval(clock.countDown, 1000);
+        clock.bigben = setInterval(clock.countDown, 1000);
     },
     stop: function() {
-    	// clearInterval(intervalId);
-    	if (time == timeleft) {
-    		clearInterval(bigben);
-            // Display countdown
-            $('#timer').html(time + "seconds");
-            time--
+
+        if (clock.time > clock.timeleft) {
+
+            $('#timer').html(clock.time + "seconds");
+
         }
-        //end of timer
-	   if (time === timeleft) {
-	   	clearInterval(bigben);
-            gameOver();
-        };
-	},
- 
+
+        if (clock.time === clock.timeleft) {
+            window.clearInterval(clock.bigben);
+            //gameOver();
+        }
+    },
 };
+clock.start();
 console.log(clock.time);
 console.log(clock.start);
 clock.countDown();
@@ -42,7 +43,8 @@ console.log(clock.stop);
 $("#submit").on("click", gameOver)
     //function gameOver on submit
 function gameOver() {
-    clearTimeout(bigben);
+	setTimeout(function(){bigben;},1000);
+    // clearTimeout(bigben);
     $("#board").hide();
     $("#menu").hide();
     $("#quiz").hide();
@@ -55,16 +57,6 @@ function gameOver() {
 
     );
 };
-// var gameOver = ["b", "b", "d", "d", "d", "b"]
-		
-//         //checking answers
-//         for (i = 1; i < total; i++) {
-//             else if (eval('q' + i) == answers[i - 1]) {
-//                 score++;
-//             } else {
-//                 wrong++;
-//             }
-//         };
 
 $("#menu").show();
 $("#board").hide();
@@ -86,3 +78,23 @@ $("#submit").click(function() {
     $("#resultsPg").show();
 });
 
+// function answers() {
+//     //user input
+//     var q1 = document.forms["quiz forms"]["q1"].value;
+//     var q2 = document.forms["quiz forms"]["q2"].value;
+//     var q3 = document.forms["quiz forms"]["q3"].value;
+//     var q4 = document.forms["quiz forms"]["q4"].value;
+//     var q5 = document.forms["quiz forms"]["q5"].value;
+//     var q6 = document.forms["quiz forms"]["q6"].value;
+// 	   var results = ["b", "b", "d", "d", "d", "b"]
+//         //checking answers
+//         for (i = 1; i < total; i++) {
+//             else if (eval('q' + i) == results[i - 1]) {
+//                 score++;
+//             } else {
+//                 wrong++;
+//             }
+// 		$("#correctAnswers").html(score);
+// 		$("#wrongAnswers").html(wrong);
+// 		$("#unanswers").html(unan);
+//         };
